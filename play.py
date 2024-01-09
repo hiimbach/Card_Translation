7b9d4f89fdbcebd5530007c9982987982b9b62b8
hiimbach <lenhobach@gmail.com>
2024-01-09 17:29:11 +0700

    mod(*) adding some stuffs

--- play.py
+++
@@ -1,8 +0,0 @@
-from utils.data.dataset import CardDataset
-train_dataset = CardDataset('data/train_261203.csv')
-test_dataset = CardDataset('data/test_261203.csv')
-
-
-# test iter train_dataset
-for i in range(1000):
-    print(train_dataset[i]['input_ids'].shape, test_dataset[i]['input_ids'].shape)
